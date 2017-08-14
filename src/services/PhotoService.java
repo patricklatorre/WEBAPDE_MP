4eeef0e089a43b4305636895310825ea52e4976d
Nate3447 <nathaniel.sierras@gmail.com>
2017-08-15 01:05:16 +0800

    Edited PhotoService.java

--- src/services/PhotoService.java
+++ src/services/PhotoService.java
@@ -1,5 +1,38 @@
 package services;
 
-public class PhotoService {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import beans.Photo;
+import db.DBPool;
+
+public class PhotoService {
+	public static void addPhoto(Photo photo) {
+		
+	}
+		
+	public static void uploadPhoto(Photo photo) {
+		
+	}
+	
+	public static void sharePhoto(Photo photo, String username) {
+		
+	}
+	
+	public static void tagPhoto(Photo photo, String tag) {
+		
+	}
+	
+	public static ArrayList<Photo> getPhotosByTag(String tag) {
+		ArrayList<Photo> photos = new ArrayList<Photo>();
+		
+		return photos;
+	}
+	
+	
+	
+	
 }
